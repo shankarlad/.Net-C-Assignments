06e3d70942a2b1f5019689a357f156f4e895f95d
shankarlad <shankarlad17@gmail.com>
2020-12-05 18:18:03 +0530

    array Assignmnet added

---
+++ Day5/Program1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee Details Accepting
+            Employee[] e = new Employee[3];
+            for (int i = 0; i < e.Length; i++)
+            {
+                Console.WriteLine("Enter the Details of Employee {0} :", i+1);
+                e[i] = new Employee();
+                e[i].getdata();
+            }
+
+            Console.WriteLine();
+
+            //employee with the heighest salary
+            Console.WriteLine("===========================");
+            decimal maxSalary = e[0].empSalary;
+            string maxSalaryEmpName = null;
+            for (int i = 1; i < e.Length; i++)
+            {
+                if (e[i].empSalary > maxSalary)
+                {
+                    maxSalary = e[i].empSalary;
+                    maxSalaryEmpName = e[i].empName;
+                }
+
+            }
+            Console.WriteLine("Employee Name : {0} ", maxSalaryEmpName);
+            Console.WriteLine("Employee Salary : {0} ", maxSalary);
+            Console.WriteLine("===========================");
+
+            Console.WriteLine();
+
+            //Empployee Search
+            Console.WriteLine("===========================");
+            Console.WriteLine("Enter the Employee No : ");
+            int eno = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < e.Length; i++)
+            {
+                if (eno == e[i].empNo)
+                {
+                    Console.WriteLine("Employee No : {0}", e[i].empNo);
+                    Console.WriteLine("Employee Name : {0}", e[i].empName);
+                    Console.WriteLine("Employee Salary : {0} ", e[i].empSalary);
+                }
+            }
+            Console.WriteLine("===========================");
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        public int empNo;
+        public string empName;
+        public decimal empSalary;
+
+        public void getdata()
+        {
+            Console.WriteLine("Enter the Emp Number:");
+            empNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Emp Name:");
+            empName = Console.ReadLine();
+            Console.WriteLine("Enter the Emp Salary:");
+            empSalary = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
---
+++ Day5/Program2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student Details Accepting
+            Student[] s = new Student[3];
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine("Enter the Details of Student {0} :", i + 1);
+                s[i] = new Student();
+                s[i].getdata();
+            }
+
+            Console.WriteLine();
+
+            //show student data
+            Console.WriteLine("===========================");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine("Details of Student {0} ", i + 1);
+                Console.WriteLine("Roll NO : {0}", s[i].rollNo);
+                Console.WriteLine("Name : {0}", s[i].name);
+                Console.WriteLine("Marks : {0}", s[i].marks);
+                Console.WriteLine("-------------------------");
+            }
+            Console.WriteLine("===========================");
+            Console.ReadLine();
+        }
+    }
+    public struct Student
+    {
+        public string name;
+        public int rollNo;
+        public decimal marks;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value != null)
+                    name = value;
+                else
+                    Console.WriteLine("Invalid");
+            }
+        }
+        public int RollNo
+        {
+            get
+            {
+                return rollNo;
+            }
+            set
+            {
+                if (value > 0)
+                    rollNo = value;
+                else
+                    Console.WriteLine("Invalid");
+            }
+        }
+
+        public int Marks
+        {
+            get 
+            { 
+                return (int)marks;
+            }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("Invalid");
+                else
+                    marks = value;
+            }
+        }
+        public Student(int RollNo = 0, string Name = null,decimal Marks = 0)
+        {
+            rollNo = RollNo;
+            name = Name;
+            marks = Marks;
+        }
+        public void getdata()
+        {
+            Console.WriteLine("Enter the roll no");
+            RollNo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter the marks");
+            Marks = Convert.ToInt32(Console.ReadLine());   
+        }
+
+    }
+
+}
